3ee38590f6b283755d18770c51080d247f622c11
moormaster <moormaster@gmx.net>
2019-11-03 19:05:32 +0100

    refactoring: extracted script code into main method

--- BtClock-Set.sh
+++ BtClock-Set.sh
@@ -6,6 +6,8 @@
 #	bt-agent
 #	stty
 
+BTCLOCK_NAME="BT Clock"
+
 get_btclock_string_from_date() {
 	if [ $# -lt 1 ]
 	then
@@ -22,7 +24,7 @@ get_btclock_string_from_date() {
 	local second=$( date --date "$datestr" +%S )
 	local dayofweek=0$(( $( date --date "$datestr" +%u ) - 1 ))
 
-	echo T=$year$month$day$hour$minute$second$dayofweek
+	echo "T=$year$month$day$hour$minute$second$dayofweek"
 }
 
 get_btclock_mac() {
@@ -44,7 +46,7 @@ get_btclock_mac() {
 		local mac=$( bluetoothctl devices | grep "$devicename" | sed "s/Device \([^ ]*\) $devicename/\1/" )
 	fi
 
-	echo $mac
+	echo "$mac"
 }
 
 is_btclock_paired() {
@@ -80,44 +82,56 @@ pair_with_btclock() {
 	return $returncode
 }
 
-BTCLOCK_NAME="BT Clock"
+main() {
+	local pin="$1"
+	local datestring="$2"
 
-pin="$1"
-
-echo -n "determining MAC of device \"BT Clock\"... " 1>&2
-mac=$( get_btclock_mac "${BTCLOCK_NAME}" )
-echo "$mac" 1>&2
-
-if [ "$mac" == "" ]
-then
-	echo "failed to discover device \"${BTCLOCK_NAME}\"" 1>&2
-	exit 1
-fi
-
-if ! is_btclock_paired "$mac"
-then
-	if [ $# -lt 1 ]
+	if [ "$datestring" == "" ]
 	then
-		echo "usage: $0 [pin]" 1>&2
-		exit 1
+		local datestring="now"
 	fi
-
-	if ! pair_with_btclock "$mac" "$pin"
+	
+	echo -n "determining MAC of device \"${BTCLOCK_NAME}\"... " 1>&2
+	local mac=$( get_btclock_mac "${BTCLOCK_NAME}" )
+	echo "$mac" 1>&2
+	
+	if [ "$mac" == "" ]
 	then
-		echo "failed to pair with device \"${BTCLOCK_NAME}\" ($mac)" 1>&2
-		exit 1
+		echo "failed to discover device \"${BTCLOCK_NAME}\"" 1>&2
+		return 1
 	fi
-fi
+	
+	if ! is_btclock_paired "$mac"
+	then
+		if [ $# -lt 1 ]
+		then
+			main-usage
+			return 1
+		fi
+	
+		if ! pair_with_btclock "$mac" "$pin"
+		then
+			echo "failed to pair with device \"${BTCLOCK_NAME}\" ($mac)" 1>&2
+			return 1
+		fi
+	fi
+	
+	# bind /dev/rfcomm0 to bt device
+	rfcomm bind rfcomm0 "$mac"
+	
+	# apply serial settings
+	stty -F /dev/rfcomm0 9600
+	
+	# send T=... string for current system time to btclock
+	get_btclock_string_from_date "$datestring" > /dev/rfcomm0
+	# read and print output for at most 3 seconds
+	cat /dev/rfcomm0 & sleep 3; kill $!
+	
+	rfcomm release rfcomm0
+}
 
-# bind /dev/rfcomm0 to bt device
-rfcomm bind rfcomm0 "$mac"
+main-usage() {
+	echo "$0 [pin] [date string]" 1>&2
+}
 
-# apply serial settings
-stty -F /dev/rfcomm0 9600
-
-# send T=... string for current system time to btclock
-get_btclock_string_from_date > /dev/rfcomm0
-# read and print output for at most 3 seconds
-cat /dev/rfcomm0 & sleep 3; kill $!
-
-rfcomm release rfcomm0
+main "$@"
